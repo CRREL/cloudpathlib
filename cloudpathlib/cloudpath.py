91d6b509977277fd0e1cd9d04f87f40aa77b2726
Michael Smith <michael.smith.erdc@gmail.com>
2025-02-28 10:05:12 -0500

    more fixing

--- cloudpathlib/cloudpath.py
+++ cloudpathlib/cloudpath.py
@@ -13,6 +13,7 @@ from pathlib import (  # type: ignore
 
 import shutil
 import sys
+from types import MethodType
 from typing import (
     overload,
     Any,
@@ -34,23 +35,43 @@ from typing import (
 from urllib.parse import urlparse
 from warnings import warn
 
+if TYPE_CHECKING:
+    from _typeshed import (
+        OpenBinaryMode,
+        OpenBinaryModeReading,
+        OpenBinaryModeUpdating,
+        OpenBinaryModeWriting,
+        OpenTextMode,
+    )
+
 if sys.version_info >= (3, 10):
     from typing import TypeGuard
 else:
     from typing_extensions import TypeGuard
+
 if sys.version_info >= (3, 11):
     from typing import Self
 else:
     from typing_extensions import Self
-if sys.version_info >= (3, 12):
+
+
+if sys.version_info < (3, 12):
+    from pathlib import _posix_flavour  # type: ignore[attr-defined] # noqa: F811
+    from pathlib import _make_selector as _make_selector_pathlib  # type: ignore[attr-defined] # noqa: F811
+    from pathlib import _PathParents  # type: ignore[attr-defined]
+
+    def _make_selector(pattern_parts, _flavour, case_sensitive=True):  # noqa: F811
+        return _make_selector_pathlib(tuple(pattern_parts), _flavour)
+
+elif sys.version_info[:2] == (3, 12):
+    from pathlib import _PathParents  # type: ignore[attr-defined]
     from pathlib import posixpath as _posix_flavour  # type: ignore[attr-defined]
     from pathlib import _make_selector  # type: ignore[attr-defined]
-else:
-    from pathlib import _posix_flavour  # type: ignore[attr-defined]
-    from pathlib import _make_selector as _make_selector_pathlib  # type: ignore[attr-defined]
+elif sys.version_info >= (3, 13):
+    from pathlib._local import _PathParents
+    import posixpath as _posix_flavour  # type: ignore[attr-defined]   # noqa: F811
 
-    def _make_selector(pattern_parts, _flavour, case_sensitive=True):
-        return _make_selector_pathlib(tuple(pattern_parts), _flavour)
+    from .legacy.glob import _make_selector  # noqa: F811
 
 
 from cloudpathlib.enums import FileCacheMode
